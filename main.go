4191b08fa8a0099d6bb3bc3f229bc73547e6d72c
Joachim Horshauge <jh.hors@gmail.com>
2025-01-26 21:25:36 +0100

    Added structured logging

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+)
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from lommeulken"))
 	})
 
+	slog.Info("Starting server...", slog.Int("port", 8080))
 	http.ListenAndServe(":8080", mux)
 }
