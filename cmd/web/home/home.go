44d817b980626ac015cfa2035d15608cfb15b407
Joachim Horshauge <jh.hors@gmail.com>
2025-01-27 13:19:24 +0100

    Removed unused test

--- cmd/web/home/home.go
+++ cmd/web/home/home.go
@@ -10,6 +10,6 @@ func HomeWebHandler(w http.ResponseWriter, r *http.Request) {
 	err := component.Render(r.Context(), w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Fatalf("Error rendering in HelloWebHandler: %e", err)
+		log.Fatalf("Error rendering in HomeWebHandler: %e", err)
 	}
 }
