2f0e06551848e2d1d4f8606f8c7a88697c6dda91
Joachim Horshauge <jh.hors@gmail.com>
2025-03-01 22:58:59 +0100

    [Added] Folding subitems to sidebar

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -12,12 +12,24 @@ func HandleHomeIndex(w http.ResponseWriter, r *http.Request) {
 	navLinks := []components.NavLink{
 		{Title: "Bliv Medlem", Url: "signup"},
 		{Title: "Nyheder", Url: "news"},
-		{Title: "Kalender", Url: "calendar"},
-		{Title: "Fangster", Url: "catches"},
-		{Title: "Konkurrencen", Url: "competition"},
-		{Title: "Hvem er vi?", Url: "about"},
 	}
-	err := home.Index(navLinks).Render(r.Context(), w)
+	navMenus := []components.NavMenu{
+		{Title: "Kalender", Links: []components.NavLink{
+			{Title: "Arrangementer", Url: "calendar"},
+			{Title: "Book udstyr", Url: "calendar/booking"}}},
+		{Title: "Fangster", Links: []components.NavLink{
+			{Title: "Seneste", Url: "catch"},
+			{Title: "Klubrekorder", Url: "club-records"},
+			{Title: "Opret fangst", Url: "new-catch"}}},
+		{Title: "Konkurrencen", Links: []components.NavLink{
+			{Title: "Igangværende", Url: "competition/current"},
+			{Title: "Tidligere", Url: "competition/previous"},
+			{Title: "Regler", Url: "competition/rules"}}},
+		{Title: "Hvem er vi?", Links: []components.NavLink{
+			{Title: "Om os", Url: "about"},
+			{Title: "Vandpleje", Url: "about/water-care"}}},
+	}
+	err := home.Index(navLinks, navMenus).Render(r.Context(), w)
 	if err != nil {
 		slog.Error("Error rendering home page", "error", err)
 		return
