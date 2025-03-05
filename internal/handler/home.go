957b84066d2f9fac377eeba03cd8c74f4eb52856
Joachim Horshauge <jh.hors@gmail.com>
2025-03-05 17:11:52 +0100

    [Added] Carousel

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 
+	cta "github.com/joachimhorshauge/lommeulken/cmd/web/components/home"
 	"github.com/joachimhorshauge/lommeulken/cmd/web/templates/home"
 )
 
@@ -29,7 +30,31 @@ func HandleHomeIndex(w http.ResponseWriter, r *http.Request) {
 			{Title: "Om os", Url: "about"},
 			{Title: "Vandpleje", Url: "about/water-care"}}},
 	}
-	err := home.Index(navLinks, navMenus).Render(r.Context(), w)
+	slides := []cta.SlideParams{
+		{
+			Title:       "Bliv Medlem",
+			Description: "Description for Bliv Medlem. This is a sample description that provides more details about the slide.",
+			Image:       "https://www.lommeulken.dk/site/gfx/pics/5771.jpg",
+			BtnText:     "Meld dig her",
+			URL:         "/signup",
+		},
+		{
+			Title:       "Fangster",
+			Description: "Description for Fangster. This is a sample description that provides more details about the slide.",
+			Image:       "https://www.lommeulken.dk/site/gfx/pics/6048.jpg",
+			BtnText:     "Se seneste",
+			URL:         "/catches?sort=latest",
+		},
+		{
+			Title:       "Om os",
+			Description: "Description for Om os. This is a sample description that provides more details about the slide.",
+			Image:       "https://www.lommeulken.dk/site/gfx/pics/6422.jpg",
+			BtnText:     "Læs mere",
+			URL:         "/about",
+		},
+	}
+
+	err := home.Index(navLinks, navMenus, slides).Render(r.Context(), w)
 	if err != nil {
 		slog.Error("Error rendering home page", "error", err)
 		return
